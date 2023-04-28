4efdab4f52caa86711aa0978eb01c314d61fa18c
Nicolas Boutet <amd3002@gmail.com>
2023-04-28 14:42:47 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,12 +5,16 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('file,user,group', [
-  ('/usr/local/bin/restic', 'root', 'staff'),
-])
+@pytest.mark.parametrize(
+    "file,user,group",
+    [
+        ("/usr/local/bin/restic", "root", "staff"),
+    ],
+)
 def test_restic_is_installed(host, file, user, group):
     restic = host.file(file)
     assert restic.exists
@@ -19,9 +23,12 @@ def test_restic_is_installed(host, file, user, group):
     assert restic.group == group
 
 
-@pytest.mark.parametrize('file,user,group,mode,content', [
-  ('/etc/restic/repo-password', 'root', 'root', 0o400, 'secret'),
-])
+@pytest.mark.parametrize(
+    "file,user,group,mode,content",
+    [
+        ("/etc/restic/repo-password", "root", "root", 0o400, "secret"),
+    ],
+)
 def test_restic_password_file_exist(host, file, user, group, mode, content):
     restic = host.file(file)
     assert restic.exists
